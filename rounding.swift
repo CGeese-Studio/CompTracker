a8226e131d6908fd38f05d6508f87b34dfb7783a
Chris Geese <Darkcyde@users.noreply.github.com>
2016-09-19 13:36:19 -0400

    Create rounding.swift
    extension to round results to 2 decimal places

---
+++ rounding.swift
@@ -0,0 +1,12 @@
+extension Double {
+    /// Rounds the double to decimal places value
+    func roundTo(places:Int) -> Double {
+        let divisor = pow(10.0, Double(places))
+        return (self * divisor).rounded() / divisor
+    }
+}
+
+
+ Example which returns Double rounded to 4 decimal places:
+let x = Double(0.123456789).roundToPlaces(4)  // x becomes 0.1235 under Swift 2
+let x = Double(0.123456789).roundTo(places: 4)  // Swift 3 version
