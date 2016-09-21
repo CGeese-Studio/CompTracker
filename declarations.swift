7094292f240c48ac180b1a2ca4a5513f20be2c8f
Chris Geese <Darkcyde@users.noreply.github.com>
2016-09-21 11:37:09 -0400

    Update declarations.swift

--- declarations.swift
+++ declarations.swift
@@ -1,1 +1,9 @@
 // running list of known constants and variables
+var newActs: Double = New Activations
+var upgrade: Double = Upgrade Activations (0.75)
+var tablet: Double = Tablet Activations (0.75)
+var prepaid = Prepaid Activations
+var spc = Sprint Phone Connect Activations
+var mbb = Mobile BroadBand Activations
+var accy = Accessory Sales
+var totalActs = Total of all activations combined
